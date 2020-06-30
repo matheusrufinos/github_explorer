ccfcedb5c5a555d6b78259900d351e8ce268d1c9
Matheus Rufino <contato@matheusrufino.com.br>
2020-06-30 18:51:01 -0300

    Create home - find repositories

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,77 @@
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImage from '../../assets/images/logo.svg';
+
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+}
+
+const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+        event.preventDefault();
+
+        if (!newRepo) {
+            setInputError('Enter author and repository name (author/repository).');
+            return;
+        }
+
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+            const repository = response.data as Repository;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (error) {
+            setInputError('Invalid repository. Check author/repository name and try again.');
+        }
+    }
+
+    return (
+        <>
+            <img src={logoImage} alt="GitHub Explorer logo" />
+            <Title>Find GitHub repositories</Title>
+
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder="Enter repository name" />
+                <button type="submit">Search</button>
+            </Form>
+
+            {inputError && <Error>{inputError}</Error>}
+
+            <Repositories>
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="test">
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login} />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
+            </Repositories>
+        </>
+    );
+}
+
+export default Dashboard;
